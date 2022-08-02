5f9ec591bee73ac391eb90c10b03488695217045
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-03 00:41:36 +0800

    useQuery updated

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,13 +8,8 @@ type Props = {
   topic?: string;
 };
 const Feed = ({ topic }: Props) => {
-  const { loading, error, data } = !topic
-    ? useQuery(GET_QUERY)
-    : useQuery(GET_ALL_POSTS_BY_TOPIC, {
-        variables: {
-          topic: topic,
-        },
-      });
+  const { loading, error, data } = useQuery(GET_QUERY);
+
   const posts: Post[] = !topic ? data?.getPostList : data?.getPostListByTopic;
   return (
     <div className="mt-5 space-y-4">
