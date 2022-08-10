912cb1c999e9b8ffed213c30700e52900c03396d
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2022-08-10 22:44:08 +0800

    Feed update.

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
-import { GET_ALL_POSTS_BY_TOPIC } from "../graphql/queries";
+import {
+  GET_ALL_POSTS_BY_TOPIC,
+  GET_SUBREDDIT_BY_TOPIC,
+} from "../graphql/queries";
 import { GET_QUERY } from "../graphql/queries";
 import Post from "./Post";
 
@@ -9,8 +12,17 @@ type Props = {
 };
 const Feed = ({ topic }: Props) => {
   const { loading, error, data } = useQuery(GET_QUERY);
+  const { data: data_topic } = useQuery(GET_ALL_POSTS_BY_TOPIC, {
+    variables: {
+      topic: topic,
+    },
+  });
 
-  const posts: Post[] = !topic ? data?.getPostList : data?.getPostListByTopic;
+  const posts: Post[] = !topic
+    ? data?.getPostList
+    : data_topic?.getPostListByTopic;
+
+  console.log(posts);
   return (
     <div className="mt-5 space-y-4">
       {posts?.map((post, i) => (
